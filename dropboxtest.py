b0e6ee916035c8e651a67393602fb51940b974f9
bkinny4148 <benjamincmckinley@gmail.com>
2020-02-20 21:47:29 -0500

    Changed filepath

--- dropboxtest.py
+++ dropboxtest.py
@@ -6,34 +6,35 @@ import json
 
 # Get Dropbox Credentials from "credentials.json"
 with open('credentials.json') as file:
-    data = json.load(file)
-    TOKEN = data["ACCESS_TOKEN"]
+	data = json.load(file)
+	TOKEN = data["ACCESS_TOKEN"]
 
 dbx = dropbox.Dropbox(TOKEN)
 
 # Function Definitions #
 def upload(dbx, filepath, upload_path):
-    with open(filepath, 'rb') as file:
-        data = file.read()
-    dbx.files_upload(data, upload_path)
+	with open(filepath, 'rb') as file:
+        	data = file.read()
+	dbx.files_upload(data, upload_path)
 
 # Format current time into string going to seconds place
 def formatTimestamp():
-    timestamp = datetime.datetime.now()
-    return timestamp.strftime("%Y-%m-%d %H:%M:%S")
+	timestamp = datetime.datetime.now()
+	return timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
 def main():
-    # Start and Format Camera Settings
-    camera = PiCamera() # new camera object
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    sleep(2) # Camera warm-up time
+	# Start and Format Camera Settings
+	camera = PiCamera() # new camera object
+	camera.resolution = (1024, 768)
+	camera.start_preview()
+	sleep(2) # Camera warm-up time
 
-    for x in range(3) : # limit calls for now
-        photo_name = formatTimestamp()
-        camera.capture(photo_name + ".jpg") # take photo
-        upload(dbx, "zebra.jpeg", "/" + photo_name + ".jpeg") # upload file
-        sleep(25)
+	for x in range(3) : # limit calls for now
+		photo_name = formatTimestamp() + ".jpeg"
+		photo_path = "/photos/" + photo_name
+		camera.capture(photo_path) # take photo
+		upload(dbx, photo_path, "/" + photo_name) # upload file
+		sleep(25)
 
 if __name__ == '__main__':
-    main()
+	main()
