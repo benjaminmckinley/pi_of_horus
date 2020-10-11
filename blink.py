4890b57ef3bca64d88c25b044bd7fca1e11106ec
bkinny4148 <45542464+bkinny4148@users.noreply.github.com>
2020-10-10 23:32:54 -0400

    Added absolute path references for crontab compat.

--- blink.py
+++ blink.py
@@ -7,22 +7,22 @@ import datetime
 import json
 import os
 
+ABS_PATH = "/home/horus/pi_of_horus/" # path to directory so that all paths are absolute
 DROPBOX_UPLOAD_PATH = "/"
 PHOTO_DIR = "photos/"
-TESTING = True
 
 # Get Dropbox Credentials from "credentials.json"
-with open('credentials.json') as file:
-	data = json.load(file)
-	TOKEN = data["ACCESS_TOKEN"]
+with open(ABS_PATH + 'credentials.json') as file:
+        data = json.load(file)
+        TOKEN = data["ACCESS_TOKEN"]
 
 dbx = dropbox.Dropbox(TOKEN)
 
 # Function Definitions #
 def upload(dbx, filepath, upload_path):
-	with open(filepath, 'rb') as file:
-        	data = file.read()
-	dbx.files_upload(data, upload_path)
+        with open(filepath, 'rb') as file:
+                data = file.read()
+        dbx.files_upload(data, upload_path)
 
 def wifi_on():
     cmd = 'ifconfig wlan0 up'
@@ -36,10 +36,10 @@ def wifi_off():
 
 # Check if there are unuploaded photos and return photo name if exists or false if none
 def upload_pic_cache():
-    for file in os.listdir(PHOTO_DIR):
+    for file in os.listdir(ABS_PATH + PHOTO_DIR):
         if file.endswith(".jpeg"):
             try: # using try-except because do not want to delete file if we cannot upload
-                photo_path = PHOTO_DIR + file
+                photo_path = ABS_PATH + PHOTO_DIR + file
                 upload(dbx, photo_path, DROPBOX_UPLOAD_PATH + file) # upload file
                 os.remove(photo_path) # remove file after upload
             except:
@@ -47,29 +47,29 @@ def upload_pic_cache():
 
 # Format current time into string going to seconds place
 def formatTimestamp():
-	timestamp = datetime.datetime.now()
-	return timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        timestamp = datetime.datetime.now()
+        return timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
 def main():
 
-    if NOT(TESTING) wifi_on()
+        wifi_on()
 
     # Start and Format Camera Settings
-	camera = PiCamera() # new camera object
-	camera.resolution = (1024, 768)
-	camera.start_preview()
-	sleep(2) # Camera warm-up time
+        camera = PiCamera() # new camera object
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        sleep(2) # Camera warm-up time
 
     # Take and Upload Photo
-	photo_name = formatTimestamp() + ".jpeg"
-	photo_path = PHOTO_DIR + photo_name
-	camera.capture(photo_path) # take photo
+        photo_name = formatTimestamp() + ".jpeg"
+        photo_path = ABS_PATH + PHOTO_DIR + photo_name
+        camera.capture(photo_path) # take photo
 
     # Try and upload all files in "photos" folder, remove as uploaded successfully
-    upload_pic_cache()
+        upload_pic_cache()
 
-    if NOT(TESTING) wifi_off()
+        wifi_off()
 
 
 if __name__ == '__main__':
-	main()
+        main()
